7c019df382a4500d47248e364bcca0b26b6538b2
Jonathan Norstrom <jon.norstrom@gmail.com>
2018-12-19 18:13:36 -0500

    small refactor

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,6 @@
 // 1. Use the power of JavaScript and the DOM API to build a clickable rock, paper, scissors game.
 document.addEventListener('DOMContentLoaded', function() {
+  var turn = [];
 
   var possibilities = [
     'rock',
@@ -8,9 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
   ];
 
   var randomPossibility = possibilities[Math.floor(Math.random() * possibilities.length)];
+
   var readComputer = function(possibilities) {
     document.getElementById('read-computer').innerHTML = 'Computer played ' + randomPossibility;
-    var turn = []
+    turn = []
     result = randomPossibility;
     turn.push({computer: result});
     console.log(turn);
@@ -20,12 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var readPlayer = function() {
     var playerSelection = document.getElementById('player-choice');
     var selectedValue = playerSelection.options[playerSelection.selectedIndex].value;
-    var textValue = playerSelection.options[playerSelection.selectedIndex].text;
-    document.getElementById('read-player').innerHTML = 'You played ' + textValue
-    return textValue;
+    document.getElementById('read-player').innerHTML = 'You played ' + selectedValue;
+    turn.push({player: selectedValue});
   }
-  var challengeComputer = document.getElementById("challenge-computer")
-  challengeComputer.addEventListener("click", readPlayer);
-  challengeComputer.addEventListener("click", readComputer);
 
+  var evalChoices = function() {
+    readComputer();
+    readPlayer();
+  }
+
+  var challengeComputer = document.getElementById("challenge-computer")
+  challengeComputer.addEventListener("click", evalChoices);
 });
